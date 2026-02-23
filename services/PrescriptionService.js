d53ada326d45fdc88f204b54df423502144b1871
azulario <azularioo@gmail.com>
2026-02-23 18:56:27 -0300

    implementa CRUD no PrescriptionService e PrescriptionController, utilizando PrescriptionRepository

--- services/PrescriptionService.js
+++ services/PrescriptionService.js
@@ -1,5 +1,30 @@
-const prescriptiontService = {
+import prescriptionRepository from "../repositories/PrescriptionRepository";
 
+const getAllPrescriptions = async () => {
+    return prescriptionRepository.getAllPrescriptions();
 }
 
-export default prescriptiontService;
+const getPrescription = async (id) => {
+    return prescriptionRepository.getPrescriptionById(id);
+}
+
+const savePrescription = async ({date, doctorId, pacientId, medication, dosage}) => {
+    return prescriptionRepository.savePrescription({date, doctorId, pacientId, medication, dosage});
+}
+
+const updatePrescription = async (id, {date, doctorId, pacientId, medication, dosage}) => {
+    return prescriptionRepository.updatePrescription(id, {date, doctorId, pacientId, medication, dosage});
+}
+
+const deletePrescription = async (id) => {
+    return prescriptionRepository.deletePrescription(id);
+}
+
+const prescriptionService = {
+    getAllPrescriptions,
+    getPrescription,
+    savePrescription,
+    updatePrescription,
+    deletePrescription
+}
+export default prescriptionService;
